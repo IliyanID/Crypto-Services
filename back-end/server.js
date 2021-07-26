decb72214b625cdaffaf9e20946bc18c8b3d181b
Iliyan Dimitrov <iliyanid2000@gmail.com>
2021-07-25 23:34:51 -0600

    Changed Server Port from 8081 to 80

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(express.static("public"));
  
-var server = app.listen(8081, function(){
+var server = app.listen(80, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
